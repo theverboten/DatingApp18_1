f1d8e15390b1e5fdad98f684f09f55eaa3289cbb
theverboten <verba1230@seznam.cz>
2023-02-19 07:17:19 +0100

    End of section 13

--- API/Extensions/ClaimsPrincipalExtensions.cs
+++ API/Extensions/ClaimsPrincipalExtensions.cs
@@ -8,6 +8,11 @@ namespace API.Extensions
     {
         public static string GetUserName(this ClaimsPrincipal user)
         {
+            return user.FindFirst(ClaimTypes.Name)?.Value;
+        }
+
+         public static string GetUserId(this ClaimsPrincipal user) //patří tam int
+        {
             return user.FindFirst(ClaimTypes.NameIdentifier)?.Value;
         }
     }
